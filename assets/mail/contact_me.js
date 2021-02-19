07ade9ee82c016b11f837128a43c9cf9e4a5a262
Patrik Kukkonen <patrik.kukkonen@gmail.com>
2021-02-19 16:49:43 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -39,7 +39,7 @@ $(function () {
                         )
                         .append("</button>");
                     $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
+                        "<strong>Viestisi on lähetetty. </strong>"
                     );
                     $("#success > .alert-success").append("</div>");
                     //clear all fields
@@ -55,9 +55,9 @@ $(function () {
                         .append("</button>");
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
-                            "Sorry " +
+                            "Pahoittelut " +
                                 firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                                ", viestin lähettämisessä ilmeni ongelma. Yritä myöhemmin uudelleen!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
